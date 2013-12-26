8b800909e1ace1f31e1e6139aaed860c934f3c72
Toxblh <toxblh@gmail.com>
2013-12-26 21:48:38 +0400

    reorganize ok!

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,43 @@
 /**
  * Created by Toxblh on 26.12.13.
  */
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Client {
+    private static final int port = 8888;
+    private static final String addr = "127.0.0.1";
+
+    public static void main(String[] ar) {
+        try {
+            InetAddress ipAdress = InetAddress.getByName(addr);
+            System.out.println("Connect to "+addr + ":"+port);
+            Socket socket = new Socket(ipAdress, port);
+            System.out.println("Open socket");
+            InputStream sin = socket.getInputStream();
+            OutputStream sout = socket.getOutputStream();
+
+            DataInputStream in = new DataInputStream(sin);
+            DataOutputStream out = new DataOutputStream(sout);
+
+            BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+            String line = null;
+            System.out.println("Type you message...");
+            System.out.println();
+
+            while (true) {
+                line = key.readLine();
+                System.out.println("Send your mess");
+                out.writeUTF(line);
+                out.flush();
+                System.out.println("Your mess send");
+                line = in.readUTF();
+                System.out.println("Server answer: " + line);
+                System.out.println();
+            }
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,45 @@
 /**
  * Created by Toxblh on 26.12.13.
  */
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private static final int port = 8888;
+
+    private void addClient (){
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(port);
+            System.out.println("Wait client..");
+            Socket socket = ss.accept();
+            System.out.println("Client finded");
+            System.out.println();
+
+            InputStream sin = socket.getInputStream();
+            OutputStream sout = socket.getOutputStream();
+
+            DataInputStream in = new DataInputStream(sin);
+            DataOutputStream out = new DataOutputStream(sout);
+
+            String line = null;
+            while (true) {
+                line = in.readUTF();
+                System.out.println("Client send: " + line);
+                out.writeUTF(line);
+                out.flush();
+                System.out.println("Wait next..");
+                System.out.println();
+            }
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+    }
 }
